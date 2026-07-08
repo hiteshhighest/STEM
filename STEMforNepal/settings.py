a5d3ef73fb31e2fa1a4d1c8ac667a6c52b912a9f
Hitesh Shah <hiteshhighest122@gmail.com>
2026-07-08 19:19:23 +0545

    Email fixes

--- STEMforNepal/settings.py
+++ STEMforNepal/settings.py
@@ -124,7 +124,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'hiteshhighest122@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwmn gzja ipsx fjjj'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = 'STEM for Nepal <hiteshhighest122@gmail.com>'
 RECIPIENT_ADDRESS = 'hiteshhighest122@gmail.com'
